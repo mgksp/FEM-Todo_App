d452f4c15a6dce144fb05ca1c606909efb0ed0f9
Prabu <prabu.mgks@gmail.com>
2022-07-09 13:14:20 +0530

    colors setup complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,31 @@ module.exports = {
       fontFamily: {
         josefinSans: ["Josefin Sans", "sans-serif"],
       },
+      colors: {
+        brightBlue: "hsl(220, 98%, 61%)",
+        checkBoxBg: {
+          from: "hsl(192, 100%, 67%)",
+          to: "hsl(280, 87%, 65%)",
+        },
+
+        lt: {
+          veryLightGray: "hsl(0, 0%, 98%)",
+          veryLightGrayishBlue: "hsl(236, 33%, 92%)",
+          lightGrayishBlue: "hsl(233, 11%, 84%)",
+          darkGrayishBlue: "hsl(236, 9%, 61%)",
+          veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+        },
+
+        dt: {
+          veryDarkBlue: "hsl(235, 21%, 11%)",
+          veryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+          lightGrayishBlue: "hsl(234, 39%, 85%)",
+          lightGrayishBlueHover: "hsl(236, 33%, 92%)",
+          darkGrayishBlue: "hsl(234, 11%, 52%)",
+          veryDarkGrayishBlue: "hsl(233, 14%, 35%)",
+          veryDarkGrayishBlue2: "hsl(237, 14%, 26%)",
+        },
+      },
     },
   },
   plugins: [],
