adac0f0945f043c981c8bed6f0350982ac95eaf5
Prabu <prabu.mgks@gmail.com>
2022-07-11 11:52:12 +0530

    added dummy todo data

---
+++ src/data/todoData.ts
@@ -0,0 +1,31 @@
+interface iTodo {
+  title: string;
+  completed: boolean;
+}
+
+export const todoData: iTodo[] = [
+  {
+    title: "Complete online JavaScript course",
+    completed: true,
+  },
+  {
+    title: "Jog around the park 3x",
+    completed: false,
+  },
+  {
+    title: "10 minutes meditation",
+    completed: false,
+  },
+  {
+    title: "Read for 1 hour",
+    completed: false,
+  },
+  {
+    title: "Pick up groceries",
+    completed: false,
+  },
+  {
+    title: "Complete Todo App on Frontend Mentor",
+    completed: false,
+  },
+];
